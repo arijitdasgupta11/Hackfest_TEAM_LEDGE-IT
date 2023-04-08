47c6485852f0a223fafa10fdc27e46eb5ac1efe1
Pratik Roy <pratikroy.profile@gmail.com>
2023-04-08 06:50:51 +0530

    candidate portal-chatbot php-static folder

---
+++ static/scripts/responses.js
@@ -0,0 +1,39 @@
+function getBotResponse(input) {
+
+    if (input == "1") {
+        return "solve metamask";
+    }
+    else if (input == "2") {
+        return "voter cant register, check your age";
+    }
+    else if (input == "3") {
+        return "maybe not eligible candidate";
+    }
+    else if (input == "4") {
+        return "Please provide the relevant details : ";
+    }
+    else if (input == "hi") {
+        return "hello :)";
+    }
+    else if (input == "hello") {
+        return "hi there!";
+    }
+    else if (input == "bye") {
+        return "have a great day";
+    }
+    else if (input == "what is your name") {
+        return "i am mr. chatbot";
+    }
+    else if (input == "who are you") {
+        return "i am mr. chatbot";
+    }
+    else if (input == "can you help me") {
+        return "sure. please select the relevant options from above.";
+    }
+    else if (input == "help") {
+        return "sure. please select the relevant options from above.";
+    }
+    else {
+        return "Sorry, cannot undertand!";
+    }
+}
